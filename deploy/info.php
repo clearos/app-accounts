ae0c20bf974c7d0ffa2e5089d40e134a028552b1
pcbaldwin <pbaldwin@clearfoundation.com>
2014-12-19 15:16:54 -0500

    Prepped for test build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'accounts';
-$app['version'] = '2.0.5';
+$app['version'] = '2.0.8';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-accounts.spec
+++ packaging/app-accounts.spec
@@ -1,7 +1,7 @@
 
 Name: app-accounts
 Epoch: 1
-Version: 2.0.5
+Version: 2.0.8
 Release: 1%{dist}
 Summary: Account Manager
 License: GPLv3
