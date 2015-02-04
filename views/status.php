ad2cc22d4d57b7b1f69017c654b1600118bcb70f
pcbaldwin <pbaldwin@clearfoundation.com>
2015-02-04 14:04:50 -0500

    Updated custom HTML for ClearOS 7 [tracker #2099]

--- views/status.php
+++ views/status.php
@@ -61,19 +61,13 @@ $openldap_directory_installed = anchor_custom('/app/openldap_directory', lang('a
 $openldap_directory_driver_install = anchor_javascript('install_and_initialize_openldap', lang('accounts_install_and_initialize_builtin_directory'));
 $openldap_driver_installed = anchor_javascript('initialize_openldap', lang('accounts_initialize_builtin_directory'));
 
-echo "<div id='accounts_wrapper'>";
-echo "<div id='accounts_configuration_widget' style='display:none;'>";
-
-echo "<input type='hidden' id='accounts_status_lock' value='off'>\n";
-echo "<input type='hidden' id='app_redirect' value='$app_redirect'>";
-
 $drivers = '';
 
 $drivers .= "
 <div id='openldap_directory_object'>
-<table cellpadding='3' cellspacing='3'>
+<table style='margin: 30px 20px 50px 20px'>
 <tr>
-    <td align='center' width='220'><img src='$openldap_logo' alt='OpenLDAP'><br><br></td>
+    <td style='width: 220px'><img src='$openldap_logo' alt='OpenLDAP'><br><br></td>
     <td>
         <p>" . lang('accounts_openldap_directory_tip') . "</p>
         <div id='openldap_directory_installed'>$openldap_directory_installed</div>
@@ -81,38 +75,46 @@ $drivers .= "
         <div id='openldap_driver_installed'>$openldap_driver_installed</div>
     </td>
 </tr>
+</table>
+</div>
+
+
+
+<div id='samba_directory_object'>
+<table style='margin: 30px 20px 50px 20px'>
 <tr>
-    <td colspan='2'>&nbsp; </td>
+    <td style='width: 220px'><img src='$samba_logo' alt='Samba Directory'><br><br></td>
+    <td>
+        <p>" . lang('accounts_samba_directory_tip') . " <strong><span style='color:red'>BETA</span></strong></p>
+        <div id='samba_directory_installed'>$samba_directory_installed_action</div>
+        <div id='samba_directory_marketplace'>$samba_directory_marketplace_action</div>
+        <div id='samba_directory_not_available'><i>$samba_directory_not_available</i></div>
+    </td>
+</tr>
+</table>
+</div>
+
+
+<div id='ad_object'>
+<table style='margin: 30px 20px 50px 20px'>
+<tr>
+    <td style='width: 220px'><img src='$ad_logo' alt='Active Directory'><br><br></td>
+    <td>
+        <p>" . lang('accounts_active_directory_connector_tip') . "</p>
+        <div id='ad_installed'>$ad_installed_action</div>
+        <div id='ad_marketplace'>$ad_marketplace_action</div>
+        <div id='ad_not_available'><i>$ad_not_available</i></div>
+    </td>
 </tr>
 </table>
 </div>
 ";
 
-// TODO: don't show Samba 4 by default yet.
-if ($base_version != 6) {
-$drivers .= "
-<div id='samba_directory_object'>
-<table cellpadding='3' cellspacing='3'>
-<tr>
-    <td align='center' width='220'><img src='$samba_logo' alt='Samba Directory'><br><br></td>
-    <td>
-        <p>" . lang('accounts_samba_directory_tip') . " <strong><span style='color:red'>BETA</span></strong></p>
-        <div id='samba_directory_installed'>$samba_directory_installed_action</div>
-        <div id='samba_directory_marketplace'>$samba_directory_marketplace_action</div>
-        <div id='samba_directory_not_available'>$samba_directory_not_available</div>
-    </td>
-</tr>
-<tr>
-    <td colspan='2'>&nbsp; </td>
-</tr>
-</table>
-</div>
-
-
+/*
 <div id='google_apps_connector_object'>
-<table cellpadding='3' cellspacing='3'>
+<table style='margin: 30px 20px 50px 20px'>
 <tr>
-    <td align='center' width='220'><img src='$google_apps_logo' alt='Google Apps Connector'><br><br></td>
+    <td style='width: 220px'><img src='$google_apps_logo' alt='Google Apps Connector'><br><br></td>
     <td>
         <p>" . lang('google_apps_connector_tip') . " <strong><span style='color:red'>BETA</span></strong></p>
         <div id='google_apps_connector_installed'>$google_apps_connector_installed_action</div>
@@ -120,38 +122,23 @@ $drivers .= "
         <div id='google_apps_connector_not_available'>$google_apps_connector_not_available</div>
     </td>
 </tr>
-<tr>
-    <td colspan='2'>&nbsp; </td>
-</tr>
 </table>
 </div>
-";
-}
+*/
 
-// TODO: implement this widget in the theme
-$drivers .= "
-<div id='ad_object'>
-<table cellpadding='3' cellspacing='3'>
-<tr>
-    <td align='center' width='220'><img src='$ad_logo' alt='Active Directory'><br><br></td>
-    <td>
-        <p>" . lang('accounts_active_directory_connector_tip') . "</p>
-        <div id='ad_installed'>$ad_installed_action</div>
-        <div id='ad_marketplace'>$ad_marketplace_action</div>
-        <div id='ad_not_available'>$ad_not_available</div>
-    </td>
-</tr>
-<tr>
-    <td colspan='2'>&nbsp; </td>
-</tr>
-</table>
-</div>
-";
+///////////////////////////////////////////////////////////////////////////////
+// Main
+///////////////////////////////////////////////////////////////////////////////
 
-// $drivers .= "</table>";
+echo "<div id='accounts_wrapper'>";
+echo "<div id='accounts_configuration_widget' style='display:none;'>";
+echo "<input type='hidden' id='accounts_status_lock' value='off'>\n";
+echo "<input type='hidden' id='app_redirect' value='$app_redirect'>";
 
 echo form_open('accounts/info');
-echo infobox_highlight(lang('accounts_account_manager_configuration'), $drivers);
+echo box_open(lang('accounts_account_manager_configuration'));
+echo $drivers;
+echo box_close();
 echo form_close();
 
 echo "</div>";
