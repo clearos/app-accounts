557ec22ace1d58561df34b64fb9561a434c9f1cd
pcbaldwin <pbaldwin@clearfoundation.com>
2015-05-29 12:30:11 -0400

    Added "configure accounts" widget [tracker #3011]

---
+++ views/please_configure.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Accounts please configure infobox.
+ *
+ * @category   apps
+ * @package    accounts
+ * @subpackage views
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2015 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/accounts/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Load dependencies
+///////////////////////////////////////////////////////////////////////////////
+
+$this->lang->load('base');
+$this->lang->load('accounts');
+
+///////////////////////////////////////////////////////////////////////////////
+// Infobox
+///////////////////////////////////////////////////////////////////////////////
+
+$options['buttons'] = array(
+    anchor_custom('/app/accounts', lang('accounts_configure_accounts_system')),
+);
+
+
+echo infobox_warning(
+    lang('base_warning'),
+    lang('accounts_configure_accounts_system_first'),
+    $options
+);
